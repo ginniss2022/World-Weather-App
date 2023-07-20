78e372b8ad310c51781a5230ec733a8533e827fd
Gabriel Inniss <gabevaughninn@gmail.com>
2023-07-19 20:29:10 -0400

    transfer

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { increment } from './Redux/slice'
+
+function App() {
+  // const count = useSelector((state) => state.counter.count)
+  // const dispatch = useDispatch();
+
+  return (
+    <>
+       <div className=''>Hello WOlrd</div>
+    </>
+  )
+}
+
+export default App
